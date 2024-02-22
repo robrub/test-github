240b8e576484b099466f7619edffd6b12117cbbd
robrub <roberto@refactormi.com>
2024-02-22 14:36:34 +0100

    backtick

---
+++ data-types/backtick.js
@@ -0,0 +1,15 @@
+let nome = "Mario";
+let eta = 30;
+
+// Usa i backtick per creare una stringa template
+let saluto = `Ciao, il mio nome è ${nome} e ho ${eta} anni.`;
+
+console.log(saluto); // Stampa "Ciao, il mio nome è Mario e ho 30 anni."
+
+let base = 5;
+let altezza = 10;
+
+// Calcola l'area di un rettangolo all'interno di una stringa template
+let area = `L'area del rettangolo è ${base * altezza}.`;
+
+console.log(area); // Stampa "L'area del rettangolo è 50."
